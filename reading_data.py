c5c24b00b16ca3c66e89fee61c5b37249dff8bc9
Oritnahmani <oritnahmani@mail.tau.ac.il>
2025-08-10 15:00:48 +0300

    save

--- reading_data.py
+++ reading_data.py
@@ -5,18 +5,21 @@ import matplotlib.pyplot as plt
 import scipy.constants
 import h5py
 
-def read_that_file(file_path):
-    with h5py.File(file_path, 'r') as f:
-        # group = f['G_tau_two_particles']
-        # data = 
+def read_GW_file(NiGWh5_path):
+    with h5py.File(NiGWh5_path, 'r') as f:
         mu = f['iter1/mu'][()]
         G_tau = f['iter1/G_tau/data'][()]
         sigma_1 = f['iter1/Sigma1'][()]
         selenergy = f['iter1/Selfenergy/data'][()]
     return(mu , G_tau ,sigma_1, selenergy)
 
+def read_H_k(inputh5_path):
+    with h5py.File(inputh5_path, 'r') as f:
+        H_k = f['HF/H-k'][()]
+    return(H_k)
 
 
 if __name__ == '__main__':
-    mu , G_tau ,sigma_1 , selenergy = read_that_file('/home/orit/VS_codes/Analysis_GW/green-mbtools/examples/NiO_GW.h5')
-    print(selenergy)
+    inputh5_path = '/home/orit//green_fun/data_analyzing_from_mbpt/input.h5'
+    mu , G_tau ,sigma_1 , selenergy = read_GW_file('/home/orit/VS_codes/Analysis_GW/green-mbtools/examples/NiO_GW.h5')
+    H_k = read_H_k(inputh5_path)
