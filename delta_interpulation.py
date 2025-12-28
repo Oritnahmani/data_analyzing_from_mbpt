9e629b29b08e274041dd0e16f9d2a620db04cfd6
Oritnahmani <oritnahmani@mail.tau.ac.il>
2025-12-28 12:55:45 +0200

    save

--- delta_interpulation.py
+++ delta_interpulation.py
@@ -6,25 +6,14 @@ import scipy.constants
 import h5py
 from green_mbtools.pesto import mb
 from mbanalysis import ir
-
 from data_analyzing_from_mbpt.Dyson_eq_analytical import G_tau
+from inchworm_stuff.redaing_txt import read_greenfunction_from_txt
+from inchworm_stuff.redaing_txt import read_delta_tau_from_txt
 
-def read_delta_tau_from_txt(delta_file,t_arr,number_of_orbitals):
-    delta_tau = np.zeros((t_arr.shape[0],number_of_orbitals, number_of_orbitals), dtype=complex)
-    with open(delta_file) as k:
-        for raw_line in k:
-            line = raw_line.strip()
-            if not line or line.startswith("#"):
-                continue
-            l_str, i_str, m_str, ij_str_re, ij_str_im = line.split()
-            l = int(l_str)
-            i = int(i_str)
-            m = int(m_str)
-            delta_tau[l, int(i), int(m)] = complex(float(ij_str_re) + 1j * float(ij_str_im))
-    return delta_tau
+
 
 def interpolation(tau_original, delta_tau_original, tau_new):
-    beta = tau_original[-1]
+    # beta = tau_original[-1]
     new_delta_tau = np.zeros((len(tau_new), delta_tau_original.shape[1], delta_tau_original.shape[2]), dtype=complex)
     for i in range(delta_tau_original.shape[1]):
         for j in range(delta_tau_original.shape[2]):
@@ -34,20 +23,31 @@ def interpolation(tau_original, delta_tau_original, tau_new):
     return new_delta_tau
 
 
-def fourier_transform(new_delta_tau, tau, beta,green_tau):
+def read_g_tau_from_txt(g_tau_file):
+    g_tau = []
+    with open(g_tau_file) as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            g_tau.append(np.array([float(x) for x in line.split()]))
+    return np.array(g_tau)
+
+
+def fourier_transform(new_delta_tau, t_arr, beta,green_tau):
     my_ir = ir.IR_factory(beta, None)
-    # tau = ir.tau_mesh              # length ir.nts
-    # mask = tau <= 10.0
 
-    # delta_ir_coeffs = my_ir.tau_to_ir(new_delta_tau, tau)
-    delta_omega = my_ir.ir_to_w(delta_ir_coeffs)
+    delta_omega = my_ir.ir_to_w(new_delta_tau, t_arr)
+    green_omega = my_ir.tau_to_w(green_tau, t_arr)
 
-
-    return delta_omega
+    return delta_omega, green_omega
 
 
 if __name__ == '__main__':
-    # number_of_orbitals = 2
+    beta = 100.0
+    number_of_orbitals = 2
     time_filename = '/home/orit/VS_codes1/example/time_intervals.txt'
     delta_file = '/home/orit/VS_codes1/example/delta.txt'
+    green_tau, t_arr = read_greenfunction_from_txt(number_of_orbitals, time_filename)
     delta_tau = read_delta_tau_from_txt(delta_file, t_arr, number_of_orbitals)
+    # new_delta_tau = interpolation(t_arr, delta_tau, t_arr)
+    # delta_omega, green_omega = fourier_transform(delta_tau, t_arr, beta, green_tau)
