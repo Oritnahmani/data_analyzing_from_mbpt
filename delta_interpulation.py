5df4b7a31d1386e59e032aa3425aa42e1a0c68b7
oritnahmani <oritnahmani@mail.tau.ac.il>
2026-01-13 01:47:43 +0200

    save

--- delta_interpulation.py
+++ delta_interpulation.py
@@ -22,15 +22,30 @@ def read_mu(NiO_GW_h5,inputh5_path):
         sigma_1 = mb.to_full_bz(sigma_1r, conj_list, ir_list, index, 1)
     return mu, sigma_1
 
-def interpolation(tau_original, delta_tau_original, tau_new):
-    # beta = tau_original[-1]
+
+
+def interpolation(tau_original, delta_tau_original, tau_new, kind="linear"):
+    tau_original = np.asarray(tau_original, dtype=float)
+    tau_new = np.asarray(tau_new, dtype=float)
+    delta_tau_original = np.asarray(delta_tau_original)
+
     new_delta_tau = np.zeros((len(tau_new), delta_tau_original.shape[1], delta_tau_original.shape[2]), dtype=complex)
+
     for i in range(delta_tau_original.shape[1]):
         for j in range(delta_tau_original.shape[2]):
-            real_interp = scipy.interpolate.interp1d(tau_original, delta_tau_original[:, i, j].real, kind='cubic', fill_value="extrapolate")
-            imag_interp = scipy.interpolate.interp1d(tau_original, delta_tau_original[:, i, j].imag, kind='cubic', fill_value="extrapolate")
+            real_interp = scipy.interpolate.interp1d(
+                tau_original, delta_tau_original[:, i, j].real,
+                kind=kind, fill_value="extrapolate", assume_sorted=True
+            )
+            imag_interp = scipy.interpolate.interp1d(
+                tau_original, delta_tau_original[:, i, j].imag,
+                kind=kind, fill_value="extrapolate", assume_sorted=True
+            )
             new_delta_tau[:, i, j] = real_interp(tau_new) + 1j * imag_interp(tau_new)
+
     return new_delta_tau
+
+
 
 
 
@@ -44,7 +59,8 @@ def interpolation(tau_original, delta_tau_original, tau_new):
 #     return np.array(g_tau)
 
 
-def fourier_transform(new_delta_tau, beta,green_tau, ir_grid_path):
+def fourier_transform( beta , ir_grid_path,green_tau,new_delta_tau ):
+
     my_ir = ir.IR_factory(beta, ir_grid_path)
     delta_omega = my_ir.tau_to_w(new_delta_tau)
     green_omega = my_ir.tau_to_w(green_tau)
@@ -118,8 +134,8 @@ def find_g_files(run_dir: Path, orbitals: int, g_glob: str):
     return expected
 
 
-def save_sigma_split_to_hdf5(h5_path, sigma_static, sigma_dynamic_iw, sigma_iw=None):
-    with h5py.File(h5_path, "w") as f:
+def save_sigma_split_to_hdf5(sigma_file, sigma_static, sigma_dynamic_iw, sigma_iw=None):
+    with h5py.File(sigma_file, "w") as f:
         f.create_dataset("Sigma_static", data=sigma_static)                 # (Norb, Norb)
         f.create_dataset("Sigma_dynamic_iw", data=sigma_dynamic_iw)         # (Nw, Norb, Norb)
         if sigma_iw is not None:
@@ -185,10 +201,13 @@ def main():
     hopping_file = run_dir / args.hopping_file
 
     green_tau, t_arr = read_greenfunction_from_txt(args.orbitals, str(time_filename), str(run_dir))
-    delta_tau = read_delta_tau_from_txt(str(delta_file), t_arr, args.orbitals)
+    delta_tau, tau_delta_original = read_delta_tau_from_txt(str(delta_file), args.orbitals,args.beta)
 
-    new_delta_tau = interpolation(t_arr, delta_tau, t_arr)
-    delta_omega, green_omega, my_ir = fourier_transform(args.beta, args.ir_grid, new_delta_tau, green_tau)
+    new_delta_tau =  interpolation(tau_delta_original, delta_tau, t_arr, kind="linear")
+
+
+
+    delta_omega, green_omega = fourier_transform(args.beta, args.ir_grid, new_delta_tau, green_tau)
 
     hopping = read_hopping_from_txt(str(hopping_file), args.orbitals)
     selfenergy_iw, sigma_static, sigma_dynamic_iw, my_ir = dyson_green_to_sigma_split_omega(
@@ -203,13 +222,18 @@ def main():
     use_weights=False)
 
 
-    save_sigma_split_to_hdf5(args.nio_gw_h5, sigma_static, sigma_dynamic_iw, sigma_iw=None)
+    sigma_file = run_dir / "selfenergy_split.h5"
+    save_sigma_split_to_hdf5(
+        sigma_file,
+        sigma_static,
+        sigma_dynamic_iw,
+        sigma_iw=selfenergy_iw
+    )
 
 
-
-    out_path = run_dir / args.out_npy
-    np.save(out_path, selfenergy_iw)
-    print(f"[save] {out_path}  shape={selfenergy_iw.shape} dtype={selfenergy_iw.dtype}")
+    # out_path = run_dir / args.out_npy
+    # np.save(out_path, selfenergy_iw)
+    # print(f"[save] {out_path}  shape={selfenergy_iw.shape} dtype={selfenergy_iw.dtype}")
 
 
 
